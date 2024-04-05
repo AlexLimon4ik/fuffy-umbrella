86d01ca4be786a75a53e924c8546f501732ea7af
AlexLimon4ik <taa.alex777@gmail.com>
2024-04-05 21:18:04 +0200

    Commit

--- Уроки/Урок 4.py
+++ Уроки/Урок 4.py
@@ -34,7 +34,6 @@
 # nick = Child()
 
 
-# Неправильний код
 # class Hello_world:
 
 #     hello = "Hello"
@@ -68,37 +67,98 @@
 # hi = Hi()
 # hi.hi_print()
 
-# Виправлений
-# class Hello_world:
-
-#     hello = "Hello"
-#     _hello = "_Hello"
-#     __hello = "__Hello"
-
+# class Hello:
 #     def __init__(self):
-#         self.world = "World"
-#         self._world = "_World"
-#         self.__world = "__World"
+#         print("Hello!")
 
-#     def printer(self):
-#         print(self.hello)
-#         print(self._hello)
-#         print(self._Hello_world__hello)
-#         print(self.world)
-#         print(self._world)
-#         print(self._Hello_world__world)
-
-# class Hi(Hello_world):
-#     def hi_print(self):
-#         print(self.hello)
-#         print(self.world)
-#         print(self._hello)
-#         print(self._world)
-#         print(self._Hello_world__hello)
-#         print(self._Hello_world__world)
+# class Hello_World(Hello):
+#     def __init__(self):
+#         super().__init__()
+#         print("World!")
         
-# hello = Hello_world()
-# hello.printer()
-# hi = Hi()
-# hi.hi_print()
+# hello_world = Hello_World()
 
+
+# class Class1:
+#     var = 20
+#     def __init__(self):
+#         self.var = 10
+
+# class Class2(Class1):
+#     def __init__(self):
+#         print(self.var)
+#         super().__init__()
+#         print(self.var)
+#         print(super().var)
+
+# hello_world = Class2()
+
+
+# class Grandparent:
+#     def about(self):
+#         print("I am GrandParent")
+#     def about_myself(self):
+#         print("I am Grandparent")
+        
+# class Parent(Grandparent):
+#     def about_myself(self):
+#         print("I am Parent")
+
+# class Child(Parent):
+#     def __init__(self):
+#         super().about()
+#         super().about_myself()
+    
+# nick = Child()
+
+
+# class Computer:
+#     def calculate(self):
+#         print("Calculating…")
+
+# class Display:
+#     def display(self):
+#         print("I display the image on the screen…")
+
+# class SmartPhone(Display, Computer):
+#     pass
+
+# iphone = SmartPhone()
+# iphone.calculate()
+# iphone.display()
+
+# Не працюючий код
+# class Computer:
+#     def __init__(self):
+#         self.memory = 128
+
+# class Display:
+#     def __init__(self):
+#         self.resolution = "4k"
+        
+# class SmartPhone(Display, Computer):
+#     def print_info(self):
+#         print(self.resolution)
+#         print(self.memory)
+
+# iphone = SmartPhone()
+# iphone.print_info()
+
+# рацюючий код
+# class Computer:
+#     def __init__(self):
+#         super().__init__()
+#         self.memory = 128
+
+# class Display:
+#     def __init__(self):
+#         super().__init__()
+#         self.resolution = "4k"
+        
+# class SmartPhone(Display, Computer):
+#     def print_info(self):
+#         print(self.resolution)
+#         print(self.memory)
+
+# iphone = SmartPhone()
+# iphone.print_info()
