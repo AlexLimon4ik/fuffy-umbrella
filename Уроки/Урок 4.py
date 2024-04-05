f13f9c8192cefc397a7e767962b1e6e33e4c4eb0
AlexLimon4ik <taa.alex777@gmail.com>
2024-04-05 23:55:00 +0200

    Commit

--- Уроки/Урок 4.py
+++ Уроки/Урок 4.py
@@ -169,10 +169,12 @@
 #         super().__init__(*args, **kwargs)
 #         self.model = model
 #         self.memory = 128
+
 # class Display:
 #     def __init__(self, *args, **kwargs):
 #         super().__init__(*args, **kwargs)
 #         self.resolution = "4k"
+
 # class SmartPhone(Display, Computer):
 #     def print_info(self):
 #         print(self.model)
