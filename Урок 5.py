eabc333205f25a174a646f5c56f4670c8f0de2e5
AlexLimon4ik <taa.alex777@gmail.com>
2024-04-06 09:33:43 +0200

    Commit

---
+++ Урок 5.py
@@ -0,0 +1,57 @@
+import requests
+# help(requests)
+
+# def function():
+#     pass
+
+# class Human:
+#     pass
+
+# rq = requests
+# func = function
+# Nick = Human
+
+# print(requests.__name__)
+# print(rq.__name__)
+# print(function.__name__)
+# print(Human.__name__)
+# print(Nick.__name__)
+# print(__name__)
+
+
+
+# intro_list = []
+
+# for method in dir(intro_list):
+#     print(method)
+
+
+# data = "text"
+
+# print(hasattr(data, 'reverse'))
+# print(hasattr(data, 'index'))
+# print(getattr(data, 'startswith'))
+# print(getattr(data, 'startswith', None))
+# print(getattr(data, 'reverse', None))
+
+
+# def f():
+#     pass
+
+# class parent:
+#     pass
+
+# class child(parent):
+#     pass
+
+
+# print(callable(data))
+# print(callable(f))
+# print(issubclass(parent, child))
+# print(issubclass(child, parent))
+
+# import sys
+# print(sys.executable)
+# print(sys.version)
+# print(sys.platform)
+# print(sys.argv)
