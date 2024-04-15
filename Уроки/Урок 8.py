c9212670b8033d25b47ceff37c4a3d8f19c03697
AlexLimon4ik <taa.alex777@gmail.com>
2024-04-15 21:24:31 +0200

    Commit

--- Уроки/Урок 8.py
+++ Уроки/Урок 8.py
@@ -1,8 +1,9 @@
-import logging
+# import logging
 
 # logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w")
 # logging.debug('debug message')
 # logging.info('info message')
+
 
 # з використанням параметру форматування 
 # logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
@@ -15,7 +16,32 @@ import logging
 #     logging.exception("Exception occurred")
 
 
-# Завдання: Записати повідомленя рівня іnfо у файл i за допомогою модуля Logging, що повідомляє користувача про успішний запуск програми
 
-logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
-logging.info('The program is working')
+# Завдання: Записати повідомленя рівня іnfо у файл i за допомогою модуля Logging, що повідомляє користувача про успішний запуск програми
+# logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w", format="We have next logging message:%(asctime)s:%(levelname)s - %(message)s")
+# logging.info('The program is working')
+
+
+# if 2+2 == 4:
+#     print("In fact, 2+2 equals 4")
+
+# assert 2 + 2 == 5, "wrong assert"
+
+
+# """
+# >>> assertion
+# result
+# """
+# if __name__ == '__main__':
+#     import doctest
+#     doctest.testmod()
+
+
+# переходить у файл тест
+# def adder(*args, **kwagrs):
+#     result = 0
+#     for _ in args:
+#         result += _
+#     for _ in kwagrs.values():
+#         result += _
+#         return result
